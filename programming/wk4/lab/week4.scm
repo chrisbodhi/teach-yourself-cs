6ff609417dec860a74127fdf2bf45b3da1c83561
Chris Boette <cboette@gmail.com>
2018-10-21 11:07:58 -0500

    Start of week 4 lab.

---
+++ programming/wk4/lab/week4.scm
@@ -0,0 +1,25 @@
+;; 3. Enter these definitions into Scheme:
+(define (make-rational num den)
+  (cons num den))
+
+(define (numerator rat)
+  (car rat))
+
+(define (denominator rat)
+  (cdr rat))
+
+(define (*rat a b)
+  (make-rational (* (numerator a) (numerator b))
+                 (* (denominator a) (denominator b))))
+
+(define (print-rat rat)
+  (word (numerator rat) '/ (denominator rat)))
+
+;; 5. Define a procedure `+rat` to add two rational numbers,
+;;    in the same style as `*rat` above.
+
+(define (+rat a b)
+  (make-rational (+ (* (numerator a) (denominator b)) (* (numerator b) (denominator a)))
+                 (* (denominator a) (denominator b))))
+
+;; 6a: Exercise 2.2 -- 
