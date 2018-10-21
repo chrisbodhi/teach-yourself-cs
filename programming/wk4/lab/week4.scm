237d24c7ecebd382bba8b8e6f8922e63121f43ff
Chris Boette <cboette@gmail.com>
2018-10-21 16:14:54 -0500

    Week 4 lab, continued.

--- programming/wk4/lab/week4.scm
+++ programming/wk4/lab/week4.scm
@@ -22,4 +22,65 @@
   (make-rational (+ (* (numerator a) (denominator b)) (* (numerator b) (denominator a)))
                  (* (denominator a) (denominator b))))
 
-;; 6a: Exercise 2.2 -- 
+;; 6a: Exercise 2.2 -- Define a constructor `make-segment` and selectors `start-segment` and `end-segment` that
+;;     define the representation of segments in terms of points...specify a constructor `make-point` and
+;;     selectors `x-point` and `y-point`...define a procedure `midpoint-segment` that takes a line segment
+;;     as argument and returns its midpoint (the point whose coordinates are the average of the coordinates
+;;     of the endpoints)
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment seg)
+  (car seg))
+
+(define (end-segment seg)
+  (cdr seg))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment seg)
+  (make-point (/ (+ (x-point (start-segment seg)) (x-point (end-segment seg))) 2)
+              (/ (+ (y-point (start-segment seg)) (y-point (end-segment seg))) 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")")
+  (newline))
+
+;; 6b: Exercise 2.3 -- Implement a representation for rectangles in a plane. In terms of your
+;;     constructors and selectors, create procedures that compute the perimeter and the area
+;;     of a given rectangle.
+
+(define (make-rect seg1 seg2)
+  (cons seg1 seg2))
+
+(define (width rect)
+  (length (car rect)))
+
+(define (height rect)
+  (length (cdr rect)))
+
+(define (length seg)
+  (define (square n) (* n n))
+  (let ((p1 (start-segment seg))
+        (p2 (end-segment seg)))
+        (sqrt (+ (square (- (x-point p1) (x-point p2)))
+                 (square (- (y-point p1) (y-point p2)))))))
+
+(define (calc-perimeter rect)
+  (* 2 (+ (width rect) (height rect))))
+
+(define (calc-area rect)
+  (* (width rect) (height rect)))
