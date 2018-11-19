4a7071bb8c5fb4354a0312dd7344bf085d84f1e0
Chris Boette <cboette@gmail.com>
2018-11-18 20:59:27 -0600

    Move bit of code from Scheme file to Markdown.

--- programming/wk4/hw/hw4.scm
+++ programming/wk4/hw/hw4.scm
@@ -13,11 +13,6 @@
 
 ;; Exercise 2.8: See ./hw4.md
 
-
-(define (sub-interval x y)
-  (make-interval (abs (- (lower-bound x) (lower-bound y)))
-                 (abs (- (upper-bound x) (upper-bound y)))))
-
 ;; Exercise 2.10: Ben Bitdiddle, an expert systems programmer, looks over Alyssa's shoulder and comments that it is not clear what it means
 ;;                to divide by an interval that spans zero. Modify Alyssa's code to check for this condition and to signal an error if it occurs.
 
