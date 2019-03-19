76a5f9ffda2bf585f2d662d259406bda4ce4f945
Chris Boette <cboette@gmail.com>
2019-03-19 12:53:32 -0500

    Add to calc program.

--- programming/wk5/hw/calc.scm
+++ programming/wk5/hw/calc.scm
@@ -12,6 +12,8 @@
 
 (define (calc-eval exp)
   (cond ((number? exp) exp)
+        ;; [x] your calculator should treat words as self-evaluating expressions
+        ((word? exp) exp)
 	((list? exp) (calc-apply (car exp) (map calc-eval (cdr exp))))
 	(else (error "Calc: bad expression:" exp))))
 
@@ -26,4 +28,24 @@
 	((eq? fn '/) (cond ((null? args) (error "Calc: no args to /"))
 			   ((= (length args) 1) (/ (car args)))
 			   (else (/ (car args) (accumulate * 1 (cdr args))))))
+; [x] first
+        ((eq? fn 'first) (if (null? args)
+                             (error "Calc: no args to first")
+                             (first (first args))))
+; [x] butfirst
+        ((or (eq? fn 'butfirst) (eq? fn 'bf)) (if (null? args)
+                                                  (error (string-append "Calc: no args to " (symbol->string fn)))
+                                                  (bf (first args))))
+; [x] last
+        ((eq? fn 'last) (if (null? args)
+                            (error "Calc: no args to last")
+                            (last (first args))))
+; [x] butlast
+        ((or (eq? fn 'butlast) (eq? fn 'bl)) (if (null? args)
+                                                 (error (string-append "Calc: no args to " (symbol->string fn)))
+                                                 (bl (first args))))
+; [x] word
+        ((eq? fn 'word) (cond ((null? args) (error "Calc: no args to word"))
+                              ((equal? 1 (length args)) (car args))
+                              (else (calc-apply 'word (cons (word (first args) (second args)) (cddr args))))))
 	(else (error "Calc: bad operator:" fn))))
