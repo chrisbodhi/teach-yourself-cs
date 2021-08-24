de625efbe9e5326cd7ea696ace10630b47ce9080
Chris Boette <cboette@gmail.com>
2021-08-23 22:08:00 -0500

    Rough histogram.

--- computer-architecture/k-and-r/ch1/1-13.c
+++ computer-architecture/k-and-r/ch1/1-13.c
@@ -1,22 +1,50 @@
 /* Ex. 1-13 Write a program to print a histogram of the lengths of words in it input. */
 #include <stdio.h>
 
+// antidisestablishmentarianism
+// 28 characters long
+#define MAX 28
+
 void fill_xs(char* arr, int count) {
   int i = 0;
-
-  while(i < count) {
+  for (i = 0; i < count; i++) {
     arr[i] = 'X';
-    i++;
   };
-  arr[count] = '\0'; // set count to 5000; without this, on the first run, got '?B' at the end of the string, and junk on subsequent runs
+  arr[count] = '\0';
 }
 
 int main() {
-  int count = 5000;
-  char xs[count];
+  int c, i, i_word;
+  int counts[MAX];
+  char xs[MAX];
 
-  fill_xs(xs, count);
-  printf("%s\n", xs);
+  i_word = 0;
+
+  // Init the counts
+  for (i = 1; i <= MAX; i++) {
+    counts[i] = 0;
+  }
+
+  while((c = getchar()) != EOF) {
+    if (c == '\n' || c == '\t') {
+      counts[i_word]++;
+      i_word = 0;
+    }
+    if (c != ' ') {
+      i_word++; // count how long the current word is
+    } else { // done counting the word
+      counts[i_word]++;
+      i_word = 0; // reset the count
+    }
+  }
+
+  printf("chars | wc\n");
+  for (i = 1; i <= MAX; i++) {
+    if (counts[i] > 0) {
+      fill_xs(xs, counts[i]);
+      printf("%d | %s\n", i, xs);
+    }
+  }
 
   return 0;
 }
